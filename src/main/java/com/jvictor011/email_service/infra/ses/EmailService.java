f7e80278f4ce6e8c55d501bac488e68ef5de324e
JVictor011 <jvictor2021@alu.ufc.br>
2024-06-25 15:57:33 -0300

    spring-boot-mail

---
+++ src/main/java/com/jvictor011/email_service/infra/ses/EmailService.java
@@ -0,0 +1,35 @@
+package com.jvictor011.email_service.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.jvictor011.email_service.adapters.EmailSenderGateway;
+import com.jvictor011.email_service.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService implements EmailSenderGateway {
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        try {
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new EmailServiceException("Falha ao enviar o email", e);
+        }
+    }
+}
